cb8dd4ed4a12e7531264266a6b5978943e750679
technogeeky <technogeeky@gmail.com>
2014-06-01 15:50:59 -0400

    fixed help screen

--- all-scanners-Mun.sh
+++ all-scanners-Mun.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-./MapSatAltitude.m -p Mun -s SAR   --sidelap-min 1.00 --sidelap-max 1.25 -r Hi -q -os csv
-./MapSatAltitude.m -p Mun -s RADAR --sidelap-min 1.00 --sidelap-max 1.25 -r Hi -q -os csv
-./MapSatAltitude.m -p Mun -s Multi --sidelap-min 1.00 --sidelap-max 1.25 -r Hi -q -os csv
+./MapSatAltitude.m -p Mun -s SAR   -r Hi -pp -q -os forum
+./MapSatAltitude.m -p Mun -s RADAR -r Hi -pp -q -os forum
+./MapSatAltitude.m -p Mun -s Multi -r Hi -pp -q -os forum
